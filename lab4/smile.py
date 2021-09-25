8039514c0b4d3b49605f7b01acb39f6863a3044e
GladyshTatiana <gladysh.tiu@phystech.edu>
2021-09-25 22:09:27 +0300

    new functions added

--- lab4/smile.py
+++ lab4/smile.py
@@ -3,6 +3,11 @@ import pygame.draw as dr
 import numpy as np
 
 
+def face(radius_face):
+    dr.circle(screen, (0, 0, 0), (200, 200), radius_face + 2)
+    dr.circle(screen, (255, 255, 0), (200, 200), radius_face)
+
+
 def eye(radius_small, radius_big, side):
     dr.circle(screen, (0, 0, 0), (side * 270 + (1 - side) * 130, 180), radius_big + 2)
     dr.circle(screen, (255, 0, 0), (side * 270 + (1 - side) * 130, 180), radius_big)
@@ -13,16 +18,13 @@ def mouth():
     dr.rect(screen, (0, 0, 0), (140, 270, 120, 20))
 
 
-def eyebrow_left(x0, y0, angle, eyebrow_length, eyebrow_width):
+def eyebrow(x0, y0, angle, eyebrow_length, eyebrow_width):
     # eyebrow_width = 20
     dr.polygon(screen, (0, 0, 0), [(x0, y0), (x0 - eyebrow_length * np.cos(angle), y0 - eyebrow_length * np.sin(angle)),
                                    (x0 - eyebrow_length * np.cos(angle) + eyebrow_width * np.sin(angle),
                                     y0 - eyebrow_length * np.sin(angle) - eyebrow_width * np.cos(angle)),
                                    (x0 + eyebrow_width * np.sin(angle), y0 - eyebrow_width * np.cos(angle))])
 
-
-def eyebrow_right():
-    dr.rect(screen, (0, 0, 0), (250, 180, 10, 20))
 
 
 pygame.init()
@@ -31,20 +33,12 @@ FPS = 30
 screen = pygame.display.set_mode((400, 400))
 
 dr.rect(screen, (255, 255, 255), (0, 0, 400, 400))
-dr.rect(screen, (0, 0, 255), (100, 100, 200, 200), 5)
-dr.polygon(screen, (255, 255, 0), [(100, 100), (200, 50),
-                                   (300, 100), (100, 100)])
-dr.polygon(screen, (0, 0, 255), [(100, 100), (200, 50),
-                                 (300, 100), (100, 100)], 5)
-dr.circle(screen, (0, 0, 0), (200, 200), 152)
-dr.circle(screen, (255, 255, 0), (200, 200), 150)
+face(150)
 eye(15, 25, 1)
 eye(13, 28, 0)
 mouth()
-# pygame.transform.rotate(eyebrow_left(), 30)
-eyebrow_left(180, 150, -1.13, 20, 120)
-eyebrow_left(220, 170, 1.24, 20, 130)
-# eyebrow_right()
+eyebrow(180, 150, -1.13, 20, 120)
+eyebrow(220, 170, 1.24, 20, 130)
 
 pygame.display.update()
 clock = pygame.time.Clock()
