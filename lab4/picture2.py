70ae1129ecee1758e8ce13ab10a2508c4fbbd0f4
timad <diudin.tv@phystech.edu>
2021-10-02 12:27:34 +0300

    added descriptions for last functions, the same for xy list instead of x and y, made font size dependent on SIZE constant,

--- lab4/picture2.py
+++ lab4/picture2.py
@@ -8,12 +8,14 @@ BLACK = (0, 0, 0)
 MOUTH_COLOR = (235, 16, 53)
 SKIN_COLOR = (245, 211, 211)
 NOSE_COLOR = (117, 97, 101)
-FIRST_HAIR_COLOR = (255, 0, 0)
+FIRST_HAIR_COLOR = (245, 233, 100)
+SECOND_HAIR_COLOR = (175, 19, 207)
+FIRST_EYE_COLOR = (150, 137, 137)
+SECOND_EYE_COLOR = (23, 160, 191)
 GREEN = (13, 74, 13)
 ORANGE = (240, 147, 26)
 BOX_COLOR = (151, 245, 100)
-
-SIZE = 1.5
+SIZE = 1
 
 
 def triangle_patch_of_hair(xy, triangle_side, angle, color_triangle):
@@ -107,7 +109,7 @@ def nose(xy, radius_nose):
 def hair(xy, radius_hair, color_hair=FIRST_HAIR_COLOR):
     """
     :param xy: (list) defines hair position space
-    :param radius_hair: matches size of triangle patches
+    :param radius_hair: matches hair patches to the size of the head
     :param color_hair: hair color
 
     draws hair for one head
@@ -156,118 +158,148 @@ def body(xy, radius_body, body_color):
                int(radius_body / 100 * 3))
 
 
-def shoulders(x, y, radius_shoulders):
+def shoulders(xy, radius_shoulders):
+    """
+    :param xy: (list) defines position of shoulder polygons
+    :param radius_shoulders: shoulder size
+
+    draws two shoulder polygons
+    """
     angle = 75
-    dr.polygon(screen, SKIN_COLOR, [(x + radius_shoulders, y + radius_shoulders),
-                                    (x + radius_shoulders + 3 * radius_shoulders * np.cos(angle / 180 * np.pi),
-                                     y + radius_shoulders - 3 * radius_shoulders * np.sin(angle / 180 * np.pi)),
-                                    (x + radius_shoulders + 3 * radius_shoulders * np.cos(angle / 180 * np.pi)
+    dr.polygon(screen, SKIN_COLOR, [(xy[0] + radius_shoulders, xy[1] + radius_shoulders),
+                                    (xy[0] + radius_shoulders + 3 * radius_shoulders * np.cos(angle / 180 * np.pi),
+                                     xy[1] + radius_shoulders - 3 * radius_shoulders * np.sin(angle / 180 * np.pi)),
+                                    (xy[0] + radius_shoulders + 3 * radius_shoulders * np.cos(angle / 180 * np.pi)
                                      + radius_shoulders / 7 * np.cos((angle + 90) / 180 * np.pi),
-                                     y + radius_shoulders - 3 * radius_shoulders * np.sin(angle / 180 * np.pi)
+                                     xy[1] + radius_shoulders - 3 * radius_shoulders * np.sin(angle / 180 * np.pi)
                                      - radius_shoulders / 7 * np.sin((angle + 90) / 180 * np.pi)),
-                                    (x + radius_shoulders + radius_shoulders / 7 * np.cos(
+                                    (xy[0] + radius_shoulders + radius_shoulders / 7 * np.cos(
                                         (angle + 90) / 180 * np.pi),
-                                     y + radius_shoulders - radius_shoulders / 7 * np.sin(
+                                     xy[1] + radius_shoulders - radius_shoulders / 7 * np.sin(
                                          (angle + 90) / 180 * np.pi))])
-    dr.polygon(screen, BLACK, [(x + radius_shoulders, y + radius_shoulders),
-                               (x + radius_shoulders + 3 * radius_shoulders * np.cos(angle / 180 * np.pi),
-                                y + radius_shoulders - 3 * radius_shoulders * np.sin(angle / 180 * np.pi)),
-                               (x + radius_shoulders + 3 * radius_shoulders * np.cos(angle / 180 * np.pi)
+    dr.polygon(screen, BLACK, [(xy[0] + radius_shoulders, xy[1] + radius_shoulders),
+                               (xy[0] + radius_shoulders + 3 * radius_shoulders * np.cos(angle / 180 * np.pi),
+                                xy[1] + radius_shoulders - 3 * radius_shoulders * np.sin(angle / 180 * np.pi)),
+                               (xy[0] + radius_shoulders + 3 * radius_shoulders * np.cos(angle / 180 * np.pi)
                                 + radius_shoulders / 7 * np.cos((angle + 90) / 180 * np.pi),
-                                y + radius_shoulders - 3 * radius_shoulders * np.sin(angle / 180 * np.pi)
+                                xy[1] + radius_shoulders - 3 * radius_shoulders * np.sin(angle / 180 * np.pi)
                                 - radius_shoulders / 7 * np.sin((angle + 90) / 180 * np.pi)),
-                               (x + radius_shoulders + radius_shoulders / 7 * np.cos((angle + 90) / 180 * np.pi),
-                                y + radius_shoulders - radius_shoulders / 7 * np.sin((angle + 90) / 180 * np.pi))],
+                               (xy[0] + radius_shoulders + radius_shoulders / 7 * np.cos((angle + 90) / 180 * np.pi),
+                                xy[1] + radius_shoulders - radius_shoulders / 7 * np.sin((angle + 90) / 180 * np.pi))],
                int(radius_shoulders / 100))
-    dr.polygon(screen, SKIN_COLOR, [(x - radius_shoulders, y + radius_shoulders),
-                                    (x - radius_shoulders - 3 * radius_shoulders * np.cos(angle / 180 * np.pi),
-                                     y + radius_shoulders - 3 * radius_shoulders * np.sin(angle / 180 * np.pi)),
-                                    (x - radius_shoulders - 3 * radius_shoulders * np.cos(angle / 180 * np.pi)
+    dr.polygon(screen, SKIN_COLOR, [(xy[0] - radius_shoulders, xy[1] + radius_shoulders),
+                                    (xy[0] - radius_shoulders - 3 * radius_shoulders * np.cos(angle / 180 * np.pi),
+                                     xy[1] + radius_shoulders - 3 * radius_shoulders * np.sin(angle / 180 * np.pi)),
+                                    (xy[0] - radius_shoulders - 3 * radius_shoulders * np.cos(angle / 180 * np.pi)
                                      - radius_shoulders / 7 * np.cos((angle + 90) / 180 * np.pi),
-                                     y + radius_shoulders - 3 * radius_shoulders * np.sin(angle / 180 * np.pi)
+                                     xy[1] + radius_shoulders - 3 * radius_shoulders * np.sin(angle / 180 * np.pi)
                                      - radius_shoulders / 7 * np.sin((angle + 90) / 180 * np.pi)),
-                                    (x - radius_shoulders - radius_shoulders / 7 * np.cos(
+                                    (xy[0] - radius_shoulders - radius_shoulders / 7 * np.cos(
                                         (angle + 90) / 180 * np.pi),
-                                     y + radius_shoulders - radius_shoulders / 7 * np.sin(
+                                     xy[1] + radius_shoulders - radius_shoulders / 7 * np.sin(
                                          (angle + 90) / 180 * np.pi))])
-    dr.polygon(screen, BLACK, [(x - radius_shoulders, y + radius_shoulders),
-                               (x - radius_shoulders - 3 * radius_shoulders * np.cos(angle / 180 * np.pi),
-                                y + radius_shoulders - 3 * radius_shoulders * np.sin(angle / 180 * np.pi)),
-                               (x - radius_shoulders - 3 * radius_shoulders * np.cos(angle / 180 * np.pi)
+    dr.polygon(screen, BLACK, [(xy[0] - radius_shoulders, xy[1] + radius_shoulders),
+                               (xy[0] - radius_shoulders - 3 * radius_shoulders * np.cos(angle / 180 * np.pi),
+                                xy[1] + radius_shoulders - 3 * radius_shoulders * np.sin(angle / 180 * np.pi)),
+                               (xy[0] - radius_shoulders - 3 * radius_shoulders * np.cos(angle / 180 * np.pi)
                                 - radius_shoulders / 7 * np.cos((angle + 90) / 180 * np.pi),
-                                y + radius_shoulders - 3 * radius_shoulders * np.sin(angle / 180 * np.pi)
+                                xy[1] + radius_shoulders - 3 * radius_shoulders * np.sin(angle / 180 * np.pi)
                                 - radius_shoulders / 7 * np.sin((angle + 90) / 180 * np.pi)),
-                               (x - radius_shoulders - radius_shoulders / 7 * np.cos((angle + 90) / 180 * np.pi),
-                                y + radius_shoulders - radius_shoulders / 7 * np.sin((angle + 90) / 180 * np.pi))],
+                               (xy[0] - radius_shoulders - radius_shoulders / 7 * np.cos((angle + 90) / 180 * np.pi),
+                                xy[1] + radius_shoulders - radius_shoulders / 7 * np.sin((angle + 90) / 180 * np.pi))],
                int(radius_shoulders / 100))
 
 
-def sleeves(x, y, radius_sleeves, sleeves_color, angle, n):
-    dr.polygon(screen, sleeves_color, [(x, y), (x + n * np.cos(angle / 180 * np.pi) * radius_sleeves,
-                                                y - np.sin(angle / 180 * np.pi) * radius_sleeves),
-                                       (x + n * np.cos((angle + 180 / 5) / 180 * np.pi) * radius_sleeves * (
+def sleeves(xy, radius_sleeves, sleeves_color, angle, n):
+    """
+    :param xy: (list) sets starting point for sleeve drawing
+    :param radius_sleeves: sleeve size
+    :param sleeves_color: sleeve color
+    :param angle: rotates sleeve polygon around starting point
+    :param n: mirrors the sleeve (for symmetry purposes)
+
+    draws one sleeve polygon
+    """
+    dr.polygon(screen, sleeves_color, [(xy[0], xy[1]), (xy[0] + n * np.cos(angle / 180 * np.pi) * radius_sleeves,
+                                                        xy[1] - np.sin(angle / 180 * np.pi) * radius_sleeves),
+                                       (xy[0] + n * np.cos((angle + 180 / 5) / 180 * np.pi) * radius_sleeves * (
                                                np.sqrt(5) + 1) / 2,
-                                        y - np.sin((angle + 180 / 5) / 180 * np.pi) * radius_sleeves * (
+                                        xy[1] - np.sin((angle + 180 / 5) / 180 * np.pi) * radius_sleeves * (
                                                 np.sqrt(5) + 1) / 2),
-                                       (x + n * np.cos((angle + 180 * 2 / 5) / 180 * np.pi) * radius_sleeves * (
+                                       (xy[0] + n * np.cos((angle + 180 * 2 / 5) / 180 * np.pi) * radius_sleeves * (
                                                np.sqrt(5) + 1) / 2,
-                                        y - np.sin((angle + 180 * 2 / 5) / 180 * np.pi) * radius_sleeves * (
+                                        xy[1] - np.sin((angle + 180 * 2 / 5) / 180 * np.pi) * radius_sleeves * (
                                                 np.sqrt(5) + 1) / 2),
-                                       (x + n * np.cos((angle + 180 * 3 / 5) / 180 * np.pi) * radius_sleeves,
-                                        y - np.sin((angle + 180 * 3 / 5) / 180 * np.pi) * radius_sleeves)
+                                       (xy[0] + n * np.cos((angle + 180 * 3 / 5) / 180 * np.pi) * radius_sleeves,
+                                        xy[1] - np.sin((angle + 180 * 3 / 5) / 180 * np.pi) * radius_sleeves)
                                        ])
-    dr.polygon(screen, BLACK, [(x, y), (x + n * np.cos(angle / 180 * np.pi) * radius_sleeves,
-                                        y - np.sin(angle / 180 * np.pi) * radius_sleeves),
-                               (x + n * np.cos((angle + 180 / 5) / 180 * np.pi) * radius_sleeves * (
+    dr.polygon(screen, BLACK, [(xy[0], xy[1]), (xy[0] + n * np.cos(angle / 180 * np.pi) * radius_sleeves,
+                                                xy[1] - np.sin(angle / 180 * np.pi) * radius_sleeves),
+                               (xy[0] + n * np.cos((angle + 180 / 5) / 180 * np.pi) * radius_sleeves * (
                                        np.sqrt(5) + 1) / 2,
-                                y - np.sin((angle + 180 / 5) / 180 * np.pi) * radius_sleeves * (
+                                xy[1] - np.sin((angle + 180 / 5) / 180 * np.pi) * radius_sleeves * (
                                         np.sqrt(5) + 1) / 2),
-                               (x + n * np.cos((angle + 180 * 2 / 5) / 180 * np.pi) * radius_sleeves * (
+                               (xy[0] + n * np.cos((angle + 180 * 2 / 5) / 180 * np.pi) * radius_sleeves * (
                                        np.sqrt(5) + 1) / 2,
-                                y - np.sin((angle + 180 * 2 / 5) / 180 * np.pi) * radius_sleeves * (
+                                xy[1] - np.sin((angle + 180 * 2 / 5) / 180 * np.pi) * radius_sleeves * (
                                         np.sqrt(5) + 1) / 2),
-                               (x + n * np.cos((angle + 180 * 3 / 5) / 180 * np.pi) * radius_sleeves,
-                                y - np.sin((angle + 180 * 3 / 5) / 180 * np.pi) * radius_sleeves)
+                               (xy[0] + n * np.cos((angle + 180 * 3 / 5) / 180 * np.pi) * radius_sleeves,
+                                xy[1] - np.sin((angle + 180 * 3 / 5) / 180 * np.pi) * radius_sleeves)
                                ], int(radius_sleeves / 30))
 
 
-def hands(x, y, radius_body, body_color):
-    shoulders(x, y, radius_body)
+def hands(xy, radius_body, body_color):
+    """
+    :param xy: (list) position of hands
+    :param radius_body: size of hands
+    :param body_color: skin color of person's hands
+
+    draws hands with sleeves and shoulders
+    """
+    shoulders(xy, radius_body)
     dr.ellipse(screen, SKIN_COLOR,
-               (x + radius_body * 1.3, y - 2.2 * radius_body, radius_body / 2, radius_body))
+               (xy[0] + radius_body * 1.3, xy[1] - 2.2 * radius_body, radius_body / 2, radius_body))
     dr.ellipse(screen, BLACK,
-               (x + radius_body * 1.3, y - 2.2 * radius_body, radius_body / 2, radius_body),
+               (xy[0] + radius_body * 1.3, xy[1] - 2.2 * radius_body, radius_body / 2, radius_body),
                int(radius_body / 100))
     dr.ellipse(screen, SKIN_COLOR,
-               (x - radius_body * 1.8, y - 2.2 * radius_body, radius_body / 2, radius_body))
+               (xy[0] - radius_body * 1.8, xy[1] - 2.2 * radius_body, radius_body / 2, radius_body))
     dr.ellipse(screen, BLACK,
-               (x - radius_body * 1.8, y - 2.2 * radius_body, radius_body / 2, radius_body),
+               (xy[0] - radius_body * 1.8, xy[1] - 2.2 * radius_body, radius_body / 2, radius_body),
                int(radius_body / 100))
-    sleeves(x + radius_body * 0.8, y + radius_body * 1.5, radius_body / 2, body_color, 20, 1)
-    sleeves(x - radius_body * 0.8, y + radius_body * 1.5, radius_body / 2, body_color, 20, -1)
+    sleeves([xy[0] + radius_body * 0.8, xy[1] + radius_body * 1.5], radius_body / 2, body_color, 20, 1)
+    sleeves([xy[0] - radius_body * 0.8, xy[1] + radius_body * 1.5], radius_body / 2, body_color, 20, -1)
 
 
-def python_is_amazing():
-    dr.rect(screen, BOX_COLOR, (0, 0, int(400 * SIZE * 1.75), int(60 * SIZE)))
-    dr.rect(screen, BLACK, (0, 0, int(400 * SIZE * 1.75), int(60 * SIZE)), int(2 * SIZE))
-    font = pygame.font.Font(None, 72)
+def python_is_amazing(k):
+    """
+    :param k: size of a banner
+
+    draws "PYTHON is  REALLY AMAZING!" banner in the top-left of a screen
+    """
+    dr.rect(screen, BOX_COLOR, (0, 0, int(750 * k), int(60 * k)))
+    dr.rect(screen, BLACK, (0, 0, int(750 * k), int(60 * k)), int(2 * k))
+    font = pygame.font.Font(None, 48 * k)
     text = font.render(
         "PYTHON is  REALLY AMAZING!", True, BLACK)
     place = text.get_rect(
-        center=(200 * SIZE * 1.75, 30 * SIZE))
+        center=(350 * k, 30 * k))
     screen.blit(text, place)
 
 
-def person(xy, k, clothes_color):
+def person(xy, k, clothes_color, hair_color, eye_color):
     """
     :param xy: (list) persons whereabouts
     :param k: persons size
     :param clothes_color: color of persons clothes
+    :param hair_color: color of persons hair
+    :param eye_color: color of persons eyes
+
     draws a person
     """
     body([xy[0] * k, xy[1] * k], 100 * k, clothes_color)
-    head([xy[0] * k, xy[1] * k], 100 * k, (245, 233, 100), (150, 137, 137))
-    hands(xy[0] * k, xy[1] * k, 100 * k, clothes_color)
+    head([xy[0] * k, xy[1] * k], 100 * k, hair_color, eye_color)
+    hands([xy[0] * k, xy[1] * k], 100 * k, clothes_color)
 
 
 pygame.init()
@@ -276,9 +308,9 @@ FPS = 30
 screen = pygame.display.set_mode((int(700 * SIZE), int(400 * SIZE)))
 
 dr.rect(screen, (212, 205, 205), (0, 0, int(700 * SIZE), int(400 * SIZE)))
-person([200, 220], SIZE, GREEN)
-person([500, 220], SIZE, ORANGE)
-python_is_amazing()
+person([200, 220], SIZE, GREEN, FIRST_HAIR_COLOR, FIRST_EYE_COLOR)
+person([500, 220], SIZE, ORANGE, SECOND_HAIR_COLOR, SECOND_EYE_COLOR)
+python_is_amazing(SIZE)
 
 pygame.display.update()
 clock = pygame.time.Clock()
